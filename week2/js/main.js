47e34f0434120a68b00cc6501b6a6920c81393ba
FlaDev <flaprumucena@byui.edu>
2022-01-13 22:53:37 -0700

    group activity week2

---
+++ week2/js/main.js
@@ -0,0 +1,23 @@
+
+function sendToDiv(){
+    let inputValue = document.getElementById('txt1').value;
+    document.getElementById('outputTxt1').innerHTML = inputValue;
+}
+
+
+function sum(){
+    let maxValue = parseInt(document.getElementById('number2').value);
+    let sum = 0;
+    while(maxValue > 0){
+        sum += maxValue
+        maxValue -= 1
+    }
+    document.getElementById('outputNum2').innerHTML = sum;
+}
+
+
+function add(){
+    let firstNum = parseInt(document.getElementById('number3-a').value);
+    let secondNum = parseInt(document.getElementById('number3-b').value);
+    document.getElementById('outputNum3').innerHTML = firstNum+secondNum;
+}
